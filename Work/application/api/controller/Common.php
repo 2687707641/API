9c667f83158d713945bc4f9133de4e5f6375234d
FHY <2687707641@qq.com>
2019-05-10 12:32:58 +0800

    用户相关完成

--- Work/application/api/controller/Common.php
+++ Work/application/api/controller/Common.php
@@ -4,6 +4,7 @@ use think\Request;
 use think\Controller;
 use think\Db;
 use think\Validate;
+use think\Image;
 
 class Common extends Controller{
 	protected $request; //用来处理参数
@@ -22,6 +23,29 @@ class Common extends Controller{
 					'user_pwd'	=> 'require|alphaDash',
 					'code' 		=> 'require|number|length:6',
 				),
+
+        'upload_head_img' => array(
+          'user_id' => 'require|number|max:2',
+          'user_icon' => 'require|image|filesize:2000000|fileExt:jpg,png,bmp,jepg',
+        ),
+
+        'set_nickname' => array(
+          'user_id' => 'require|number|max:2',
+          'nickname' => 'require|max:20|alphaDash',
+        ),
+
+        'change_pwd'  => array(
+          'user_phone'    => 'require',  
+          'user_ini_pwd' => 'require', //原密码     
+          'user_new_pwd'     => 'require',
+        ),
+
+        'find_pwd'      => array(
+          'user_phone'    => 'require',  
+          'code' => 'require|number|length:6',
+          'user_pwd'     => 'require',
+        ),
+        
 			),
 
 			'Code' => array(
@@ -30,13 +54,40 @@ class Common extends Controller{
 					'is_exist'  => 'require|number|length:1',
 				),
 			),
+
+      'Cate' => array(
+        'cate_add' => array(
+            'cate_name' =>'require',
+            'cate_pid' =>'require|number',
+        ),
+
+        'cate_list' => array(
+        ),
+
+        'cate_delete' => array(
+            'cate_id' => 'require',
+        ),
+      ),
+
+      'Book' => array(
+        'book_add' => array(
+            'book_name' => 'require|max:20',
+            'book_price' => 'require|number',
+            'book_pid' => 'require|number',
+            'book_num' => 'require|number',
+        ),
+
+        'book_list' => array(
+            'cate_id' => 'require|number',
+        ),
+      ),
 	);
 
 	protected function _initialize(){
         parent::_initialize();
         $this->request = Request::instance();
         // $this->check_time($this->request->only(['time']));//验证时间戳
-       $this->params =  $this->check_params($this->request->except(['time']));//过滤参数
+       $this->params =  $this->check_params($this->request->param(true));//过滤参数
 
     }
 
@@ -55,9 +106,8 @@ class Common extends Controller{
     	}
     }
 
-    /**
+  /**
 	* api 数据返回
-	 
 	 * @param  [int] $code [结果码 200:正常/4**数据问题/5**服务器问题]
 	 * @param  [string] $msg  [接口要返回的提示信息]
 	 * @param  [array]  $data [接口要返回的数据]
@@ -69,7 +119,7 @@ class Common extends Controller{
    		$return_data['$msg'] = $msg;
    		$return_data['$data'] = $data;
    		/*            返回错误信息，并终止脚本       */
-   		echo json_encode($return_data);die;  
+   		echo json_encode($return_data,JSON_UNESCAPED_UNICODE);die;  
 
    	}
 
@@ -113,7 +163,7 @@ class Common extends Controller{
    				}
    				break;
 
-   			/*		手机号不应该在数据库中	*/
+   			/*		手机号应该在数据库中	*/
    			case '1':
    				if(!$phone_res){
    					$this->return_msg(400,'此手机号不存在!');
@@ -137,5 +187,108 @@ class Common extends Controller{
 		/*		验证码只验证一次		*/
 		// session($user_name.'_code',null);
 	}
+
+
+  public function upload_file($file,$type=''){
+    // 将文件移动到根目录的public的uploads文件夹里，DS会用当前时间创建文件夹
+    $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads'); 
+    if($info){
+      // dump($info->getSavename());die();
+      $path = '/uploads/' . $info->getSavename();
+      /*    裁剪图片    */
+      if(!empty($type)){
+        $this->image_edit($path,$type);
+        return str_replace('\\', '/', $path);
+      }      
+    }else{
+      $this->return_msg(400, $file->getError());
+    }
+  }
+
+  public function image_edit($path, $type) {
+      $image = Image::open(ROOT_PATH . 'public' . $path);
+      switch ($type) {
+      case 'head_img':
+          $image->thumb(200, 200, Image::THUMB_CENTER)->save(ROOT_PATH . 'public' . $path);
+          break;
+      }
+  }
+
+
+
+
+  /*    检查栏目是否存在    */
+  public function check_pid($pid){
+      $pid_res = db('cate')->where('cate_id',$pid)->find();
+      if(!$pid_res){
+          $this->return_msg(400,'该上级栏目不存在!');
+      }
+  }
+
+  /*    无限极栏目打印     */
+  public function sortCate($data,$pid=0){
+      static $arr=[];//定义一个空的数组
+      foreach ($data as $k => $v) { //k是id  v是他的值
+          if ($v['cate_pid']==$pid) {//如果是顶级栏目就将其放进数组里
+              $arr[]=$v;
+              $this->sortCate($data,$v['cate_id']);//找到顶级栏目后找其他栏目
+          }
+      }
+     return $arr;
+  }
+
+
+  /*    删除栏目    */
+  public function getChildIds($id){
+    $data = db('cate')->select();//得到所有栏目的信息
+    return $this->_getChildIds($data,$id);//运行下面私有方法 传参 所有栏目信息 和 传参栏目id
+  }
+
+  private function _getChildIds($data,$id){
+    static $arr=array();//创建静态数组
+    foreach ($data as $k => $v) {
+        if ($v['cate_pid']==$id) {//所有栏目的pid 等于当前栏目id 则为子栏目
+            $arr[]=$v['cate_id'];//获取其子栏目id 放到静态数组里面
+            $this->_getChildIds($data,$v['cate_id']);//继续往下找  寻找子栏目的子栏目
+        }
+    }
+    return $arr;
+  }
+
+  /*    检测书籍是否重名    */
+  public function check_repeat($name){
+    $res = db('book')
+          ->where('book_name', $name)
+          ->find();
+    if($res){
+      $this->return_msg(400,'该书籍已存在!');
+    }
+  }
+
+
+  /**/
+  public function select_list($id){
+
+    $res = db('cate')
+          ->where('cate_id', $id)
+          ->find();
+
+    /*    如果是顶级栏目就返回他的下级栏目,如果不是顶级栏目就返回该栏目下的所有书籍信息*/
+    if($res['cate_pid']==0){
+        $db_res = db('cate')
+                  ->where('cate_pid', $id)
+                  ->select();    
+    }else{
+      $db_res = db('book')
+                ->where('book_pid', $id)
+                ->select();
+    }
+     return $db_res;
+  }
+
+
+
+
+
 
 }
--- Work/application/route.php
+++ Work/application/route.php
@@ -6,11 +6,44 @@ Route::domain('api','api');
 //获取验证码
 Route::get('code/:time/:username/:is_exist','code/get_code');
 
-//
-Route::post('user','user/login');
-
+/************用户管理**********************/
 // 用户注册
 Route::post('user/register','user/register');
 
 // 用户登录
 Route::post('user/login','user/login');
+
+// 用户找回密码
+Route::post('user/find_pwd','user/find_pwd');
+
+// 用户上传头像
+Route::post('user/icon','user/upload_head_img');
+
+// 用户修改昵称
+Route::post('user/nickname','user/set_nickname');
+
+// 用户修改密码
+Route::post('user/change_pwd','user/change_pwd');
+
+
+
+
+/************栏目管理*******************/
+
+// 添加栏目(添加分类)
+Route::post('cate/add','cate/cate_add');
+
+// 栏目列表(查询有哪些分类，以便于添加二级分类)
+Route::get('cate/list','cate/cate_list');
+
+// 删除栏目
+Route::get('cate/del/:cate_id','cate/cate_delete');
+
+
+/**************书籍管理**********************/
+
+//添加商品(书籍)
+Route::post('book/add','book/book_add');
+
+//查看该栏目下的所有书籍
+Route::get('book/book_list/:time/:cate_id','book/book_list'); 
